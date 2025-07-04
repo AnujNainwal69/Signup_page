1d7248b5fb24fe0e65927ac96596893f7a5b763b
AnujNainwal69 <anainwal7@gmail.com>
2025-07-04 21:29:41 +0530

    Building Signup Page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignupForm from './SignupForm'; 
+
+function App() {
+  return (
+    <div className="bg-gray-50 min-h-screen flex items-center justify-center">
+      <SignupForm />
+    </div>
+  );
+}
+
+export default App;
